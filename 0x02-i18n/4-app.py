0da691dc2a6fd8e5f54ae1a505df289a6d542958
Willmuseve <willmuseve@gmail.com>
2024-04-09 18:23:26 +0000

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,57 +1,46 @@
 #!/usr/bin/env python3
-
 """
-A route in oython flask
-Import and configure flask babel
+a route using flask.
+Import and configure flask babel.
 """
 
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
-
-
-app = Flask(__name__)
+from flask import Flask, render_template, request
 
 
 class Config:
-    """
-    A class defining a basic
-    babel configurations
-    """
-
+    """Basic babel configurations"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """
-    A function that gets the best match for
-    user locale.
-    """
-    # Checks the 'locale' parameter is present in the request URL
-    locale = request.args.get("locale")
+def get_locale() -> str:
+    """Returns locale"""
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
 
-    if locale and locale in app.config["LANGUAGES"]:
-        return locale
-
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-@app.route("/", methods=["GET"])
-def render_index():
-    """
-    A function that renders the html
-    template index.html
-    """
-    return render_template("4-index.html")
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return (query_table['locale'])
+    return (request.accept_languages.best_match(app.config["LANGUAGES"]))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/')
+def get_index() -> str:
+    """renders html template"""
+    return (render_template('4-index.html'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
