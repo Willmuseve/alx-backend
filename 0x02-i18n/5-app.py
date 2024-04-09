f5fa9e5386a12f7920bfd39f5c4b1f528a6eaa8a
Willmuseve <willmuseve@gmail.com>
2024-04-09 18:40:39 +0000

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Route using flask.
+Import and configure flask babel.
+"""
+
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+from typing import Dict, Union
+
+
+class Config:
+    """Class for Flask Babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """A function that returns a user based on user id"""
+    login_id = request.args.get('login_as')
+    if login_id:
+        return (users.get(int(login_id)))
+    return (None)
+
+
+@app.before_request
+def before_request() -> None:
+    """Function that executes before all other functions"""
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Returns locale"""
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return (locale)
+    return (request.accept_languages.best_match(app.config["LANGUAGES"]))
+
+
+@app.route('/')
+def get_index() -> str:
+    """Returns the index.html template """
+    return (render_template('5-index.html'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
